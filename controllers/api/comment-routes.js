ec4fa6a9d1e2e9550c22c47c3cf231a4a9a87475
Niklas Solomon <niklassolomon@gmail.com>
2022-09-19 18:58:07 -0700

    built create comment route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -11,3 +11,17 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(commentData => res.json(commentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    };
+});
