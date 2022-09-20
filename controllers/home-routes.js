e7cd0dbc752d3871f98f749cc68fdc868b786ba5
Niklas Solomon <niklassolomon@gmail.com>
2022-09-19 19:49:08 -0700

    built route to render single post page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,4 +35,44 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => {
+        if (!postData) {
+            res.status(404).json({ message: 'Invalid ID' });
+            return;
+        }
+        const post = postData.get({ plain: true });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
