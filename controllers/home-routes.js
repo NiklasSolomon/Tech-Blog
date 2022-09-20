9466864afe0478ef4a5ca06c9317b81ca6a09d16
Niklas Solomon <niklassolomon@gmail.com>
2022-09-19 19:40:24 -0700

    built route to render all posts to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,37 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const sequelize = require('../config/connection');
 
-
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => {
+        const post = postData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
