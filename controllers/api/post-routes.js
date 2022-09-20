cc87be342def1077a21404d66473339e3d2a56b2
Niklas Solomon <niklassolomon@gmail.com>
2022-09-19 18:35:37 -0700

    built create post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -62,3 +62,16 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_content: req.body.post_content,
+        user_id: req.session.user_id
+    })
+    .then(postData => res.json(postData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
