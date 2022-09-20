805ec077800a2ce293ed91c53eed8ce2c6259abb
Niklas Solomon <niklassolomon@gmail.com>
2022-09-19 19:16:57 -0700

    created route to display posts to logged in users

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+const sequelize = require('../config/connection');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => {
+        const post = postData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { post, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const sequelize = require('../config/connection');
+
+
+
+module.exports = router;
