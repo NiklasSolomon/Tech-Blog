9938a2c0dcc7b5377ee497ce54bb962b4aabdcf2
Niklas Solomon <niklassolomon@gmail.com>
2022-09-19 18:09:38 -0700

    created get all posts route

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+const sequelize = require('../../config/connection');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id',
+                    'post_content',
+                    'title',
+                    'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => res.json(postData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
