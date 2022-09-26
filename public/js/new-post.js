2e237cdad3bf353730774cc650a4a46cc69d2811
Niklas Solomon <niklassolomon@gmail.com>
2022-09-25 18:53:44 -0700

    created new post function

---
+++ public/js/new-post.js
@@ -0,0 +1,28 @@
+const titleEl = document.querySelector('input[name="title"]');
+const contentEl = document.querySelector('input[name="content"]');
+
+const newPost = async (event) => {
+    event.preventDefault();
+
+    const title = titleEl.value.trim();
+    const content = contentEl.value.trim();
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+          title,
+          content
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+};
+
+document.querySelector('#new-post').addEventListener('submit', newPost);
