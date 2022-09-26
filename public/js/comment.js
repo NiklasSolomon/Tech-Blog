75beb3d6dc7015069976b6b1f58f85b25732aeb9
Niklas Solomon <niklassolomon@gmail.com>
2022-09-25 19:14:07 -0700

    created comment function

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,1 +1,31 @@
-const commentEl = document.querySelector('textarea[name="comment-text"]');
+const comment_text = document.querySelector('textarea[name="comment-text"]');
+const post_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+const commentHandler = async (event) => {
+    event.preventDefault();
+
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+
+        } else {
+            alert(response.statusText);
+            document.querySelector('#comment-form').style.display = "block";
+        }
+    }
+};
+
+document.querySelector('.comment').addEventListener('submit', commentHandler);
